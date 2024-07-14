d6606f4789a3129a9fd213ada0a5eb17e60de8ca
Ibaliqbal <iqbalmuthahhary@gmail.com>
2024-07-14 23:47:02 +0700

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      maxWidth: {
+        layout: "1600px",
+      },
+    },
+  },
+  plugins: [],
+};
